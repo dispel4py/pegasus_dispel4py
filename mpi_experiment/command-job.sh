583b66731d07f18d4e03b04b3ce1a4554ba9431d
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2016-04-14 20:16:33 -0700

    change in the command username

--- mpi_experiment/command-job.sh
+++ mpi_experiment/command-job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-ssh -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi mpirun@mpi_head 'mpiexec -n 6 -hostfile machines dispel4py mpi dispel4py.examples.graph_testing.pipeline_test -i 10' 
+ssh -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head 'mpiexec -n 6 -hostfile machines dispel4py mpi dispel4py.examples.graph_testing.pipeline_test -i 10' 
