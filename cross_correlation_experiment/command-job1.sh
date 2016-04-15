5a9192af86ed0f1a58a0e152627b4f266f9e5e18
rosafilgueira <rosa.filgueira@ed.ac.uk>
2016-04-15 23:45:19 +0000

    Cross_correlation jobs fixed

--- cross_correlation_experiment/command-job1.sh
+++ cross_correlation_experiment/command-job1.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -x
-export PYTHON_PATH=.
-cd /home/tutorial/dispel4py
-
-export DISPEL4PY_XCORR_STARTTIME=2015-04-06T06:00:00.000
-export DISPEL4PY_XCORR_ENDTIME=2015-04-06T07:00:00.000
-
-rm -rf  tc_cross_correlation/OUTPUT/DATA
-rm -rf  tc_cross_correlation/OUTPUT/XCORR
-
-mkdir  tc_cross_correlation/OUTPUT/DATA
-mkdir  tc_cross_correlation/OUTPUT/XCORR
-
-
-#dispel4py multi tc_cross_correlation/realtime_prep.py -f tc_cross_correlation/realtime_xcorr_input.jsn -n 4	
-python -m dispel4py.new.processor multi tc_cross_correlation/realtime_prep.py -f tc_cross_correlation/realtime_xcorr_input.jsn -n 4
-
+ssh -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head './command-preproces.sh' 
+scp -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head:/home/tutorial/preprocess_data.zip .
--- cross_correlation_experiment/command-job2.sh
+++ cross_correlation_experiment/command-job2.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -x
 export PYTHON_PATH=.
+
+
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
+
+
+mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
+
+cp /home/tutorial/work/tutorial/pegasus/dispel4py/20160415T230445+0000/preprocess_data.zip /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+cd /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+unzip preprocess_data.zip
+
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/preprocess_data.zip
+
 cd /home/tutorial/dispel4py
 
-export DISPEL4PY_XCORR_STARTTIME=2015-04-06T06:00:00.000
-export DISPEL4PY_XCORR_ENDTIME=2015-04-06T07:00:00.000
-
-#dispel4py multi realtime_xcorr.py -n 4
 python -m dispel4py.new.processor multi tc_cross_correlation/realtime_xcorr.py -n 4 
