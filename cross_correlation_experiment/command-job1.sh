63666c96030bab853e64e654e1013f4e8692d7d7
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2016-04-14 19:19:30 -0700

    command-job1.sh

--- cross_correlation_experiment/command-job1.sh
+++ cross_correlation_experiment/command-job1.sh
@@ -6,11 +6,11 @@ cd /home/tutorial/dispel4py
 export DISPEL4PY_XCORR_STARTTIME=2015-04-06T06:00:00.000
 export DISPEL4PY_XCORR_ENDTIME=2015-04-06T07:00:00.000
 
-rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA
-rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR
+rm -rf  tc_cross_correlation/OUTPUT/DATA
+rm -rf  tc_cross_correlation/OUTPUT/XCORR
 
-mkdir /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA
-mkdir /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR
+mkdir  tc_cross_correlation/OUTPUT/DATA
+mkdir  tc_cross_correlation/OUTPUT/XCORR
 
 
 #dispel4py multi tc_cross_correlation/realtime_prep.py -f tc_cross_correlation/realtime_xcorr_input.jsn -n 4	
