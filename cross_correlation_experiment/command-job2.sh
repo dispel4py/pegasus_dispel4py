3d8792cee10c0401f06f48c281f65863cded0e9a
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2016-04-19 14:57:54 -0700

    improvment of command-job2.sh

--- cross_correlation_experiment/command-job2.sh
+++ cross_correlation_experiment/command-job2.sh
@@ -10,7 +10,7 @@ rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
 mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
 mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
 
-cp /home/tutorial/work/tutorial/pegasus/dispel4py/20160415T230445+0000/preprocess_data.zip /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+cp `pwd`/preprocess_data.zip /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
 cd /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
 unzip preprocess_data.zip
 
@@ -18,4 +18,4 @@ rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/preprocess_data
 
 cd /home/tutorial/dispel4py
 
-python -m dispel4py.new.processor multi tc_cross_correlation/realtime_xcorr.py -n 4 
+python -m dispel4py.new.processor multi tc_cross_correlation/realtime_xcorr.py -n 4
