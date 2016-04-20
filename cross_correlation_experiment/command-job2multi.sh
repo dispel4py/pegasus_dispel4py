bb6790f6bff83b3ae033c976d0f2e5bb94e12ea8
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2016-04-20 14:46:26 -0700

    command for running crosscorrelation localy with multi

---
+++ cross_correlation_experiment/command-job2multi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+export PYTHON_PATH=.
+
+
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
+
+
+mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+mkdir -p /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/XCORR/
+
+cp `pwd`/preprocess_data.zip /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+cd /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/
+unzip preprocess_data.zip
+
+rm -rf /home/tutorial/dispel4py/tc_cross_correlation/OUTPUT/DATA/preprocess_data.zip
+
+cd /home/tutorial/dispel4py
+
+python -m dispel4py.new.processor multi tc_cross_correlation/realtime_xcorr.py -n 4
