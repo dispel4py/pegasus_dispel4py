f1bfe258ba7d56426414c2116b15a7b3f01f5aae
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2016-05-02 21:41:33 -0700

    rename cross_correlation command-job1.sh

--- cross_correlation_experiment/command-job1.sh
+++ cross_correlation_experiment/command-job1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-ssh -o StrictHostKeyChecking=no -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head './command-preproces.sh' 
+ssh -o StrictHostKeyChecking=no -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head './command-preprocess.sh' 
 scp -o StrictHostKeyChecking=no -i /home/tutorial/docker.openmpi/ssh/id_rsa.mpi tutorial@mpi_head:/home/tutorial/preprocess_data.zip .
